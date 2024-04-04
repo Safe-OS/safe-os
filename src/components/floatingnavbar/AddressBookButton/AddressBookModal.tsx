163f81f3bb44b778c4711ff63cf96fbd2a0782d2
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-04-04 23:26:33 +0200

    Update AddressBookModal.tsx

--- src/components/floatingnavbar/AddressBookButton/AddressBookModal.tsx
+++ src/components/floatingnavbar/AddressBookButton/AddressBookModal.tsx
@@ -2,7 +2,7 @@ import { type ReactElement } from 'react'
 import AddressBookTable from '@/components/address-book/AddressBookTable'
 import ModalDialog from '@/components/common/ModalDialog'
 
-const QrModal = ({ onClose }: { onClose: () => void }): ReactElement => {
+const AddressBook = ({ onClose }: { onClose: () => void }): ReactElement => {
   return (
     <ModalDialog open dialogTitle="Address book" onClose={onClose}>
       <DialogContent>
