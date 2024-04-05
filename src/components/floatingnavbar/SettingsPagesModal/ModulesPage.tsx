1eede5db10f381f49ec77165198476f46273c436
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-04-05 23:01:27 +0200

    Create ModulesPage.tsx

---
+++ src/components/floatingnavbar/SettingsPagesModal/ModulesPage.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import { Grid } from '@mui/material'
+import SafeModules from '@/components/settings/SafeModules'
+import TransactionGuards from '@/components/settings/TransactionGuards'
+import { FallbackHandler } from '@/components/settings/FallbackHandler'
+
+const Modules: NextPage = () => {
+  return (
+    <>
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <SafeModules />
+          </Grid>
+
+          <Grid item>
+            <TransactionGuards />
+          </Grid>
+
+          <Grid item>
+            <FallbackHandler />
+          </Grid>
+        </Grid>
+    </>
+  )
+}
+
+export default Modules
