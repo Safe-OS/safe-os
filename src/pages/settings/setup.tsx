bae0efc7d6bb4c6bcef4d213248ff4708017fcf9
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-04-09 19:54:41 +0200

    Update setup.tsx

--- src/pages/settings/setup.tsx
+++ src/pages/settings/setup.tsx
@@ -17,7 +17,7 @@ const Setup: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{'Safe{Wallet} – Settings – Setup'}</title>
+        <title>{'SafeOS – Settings – Setup'}</title>
       </Head>
 
       <main>
