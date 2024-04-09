875e49206ef653479ace29de36d0b2219dce5377
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-04-09 19:17:06 +0200

    Update modules.tsx

--- src/pages/settings/modules.tsx
+++ src/pages/settings/modules.tsx
@@ -3,17 +3,14 @@ import Head from 'next/head'
 import { Grid } from '@mui/material'
 import SafeModules from '@/components/settings/SafeModules'
 import TransactionGuards from '@/components/settings/TransactionGuards'
-import SettingsHeader from '@/components/settings/SettingsHeader'
 import { FallbackHandler } from '@/components/settings/FallbackHandler'
 
 const Modules: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{'Safe{Wallet} – Settings – Modules'}</title>
+        <title>{'SafeOS – Settings – Modules'}</title>
       </Head>
-
-      <SettingsHeader />
 
       <main>
         <Grid container direction="column" spacing={2}>
