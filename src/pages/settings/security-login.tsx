59924f7c7758ff88bb8c58630f5090b91265de19
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-04-09 19:14:49 +0200

    Update security-login.tsx

--- src/pages/settings/security-login.tsx
+++ src/pages/settings/security-login.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-import SettingsHeader from '@/components/settings/SettingsHeader'
 import SecurityLogin from '@/components/settings/SecurityLogin'
 
 const SecurityLoginPage: NextPage = () => {
